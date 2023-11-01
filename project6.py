32ed2738bb77de88c5e9243bf9241c4cb6880c5d
Venus Velazquez <venus.dv10@gmail.com>
2023-11-01 09:36:57 -0400

    set up project6 file

---
+++ project6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#   Author:		Venus Velazquez
+#	Major:		CS
+#	Due Date:	Nov. 7, 2023
+#	Course:		CSC328 020
+#	Assignment:	Socket programming
+#	Filename:	project6.py
+#	Purpose: 	This server program sends word data to connected clients
+
+import sys
+import socket
+
+# Description:  
+# Parameters:   
+# Returns: 
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
