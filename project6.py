336d419968355ca168b7ad8cb10997711c7c0079
Venus Velazquez <venus.dv10@gmail.com>
2023-11-06 23:07:30 -0500

    Testing code

--- project6.py
+++ project6.py
@@ -7,12 +7,37 @@
 #	Filename:	project6.py
 #	Purpose: 	This server program sends word data to connected clients
 
+import signal
 import sys
 import socket
+import struct
+import random
 
 # Description:  
 # Parameters:   
 # Returns: 
+
+def interrupt_handler(s):
+    print("Server stopped.")
+    s.close()
+    sys.exit(0)
+
+def create_word_packets(file_in):
+
+    with open(file_in, 'r') as file:
+        words = file.read().splitlines()
+    num_packets = random.randint(1, 10)
+    packets = []
+
+    for _  in range(num_packets):
+        word = random.choice(words)
+        word_length = len(word)
+        word_bytes = struct.pack('>H', word_length)
+        word_data = word.encode('ascii')
+        word_packet = word_bytes + word_data
+        packets.append(word_packet)
+
+    return(packets)
 
 
 def main():
@@ -21,16 +46,29 @@ def main():
         exit('Usage: server <port>')
     try:
         with socket.socket() as s:
-            s.bind(("127.0.0.1", int(sys.argv[1])))
+
+            print("args: ", sys.argv[0], sys.argv[1])
+
+            s.bind(("0.0.0.0", int(sys.argv[1])))
             s.listen()
+
+            signal.signal(signal.SIGINT, interrupt_handler(s))
+
             while True:
                 (client_sock, client_add) = s.accept()
 
-                while True:
-                    data = client_sock.recv(1024)
-                    if data != b'':
-                        client_sock.sendall(data)
-                        break
+                file_in = "words.txt"
+                word_packets = create_word_packets(file_in)
+
+                for packet in word_packets:
+                    print(packet)
+
+                    client_sock.sendall(packet)
+                
+                print(f"Sent {len(word_packets)} word packets to {client_add}")
+
+                client_sock.close()
+
     except OSError as err:
         exit(f'{err}')
 
